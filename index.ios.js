0caacf8fef592966ae163125e3c7cb6efb4c65de
ichiki-sato1023 <ichiki_sato1023@r.recruit.co.jp>
2017-03-27 14:04:44 +0900

    clean directory

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,6 @@
  */
 
 import React, { AppRegistry } from 'react-native';
-import App from './src/containers/app';
+import App from './src/components/app';
 
 AppRegistry.registerComponent('AwesomeProject', () => App);
